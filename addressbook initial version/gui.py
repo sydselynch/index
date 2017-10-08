3c9cab0a7043eafb7dbd330e3da81a33abe0c884
Syd <slynch2@uoregon.edu>
2017-10-08 16:24:30 -0700

    functioning start window

--- addressbook initial version/gui.py
+++ addressbook initial version/gui.py
@@ -33,7 +33,6 @@ class Start():
     def initializeUI(self):
 
         #Initialize buttons
-        #TODO add functions to buttons
         newButton = Button(self.root, text="New", width=20, command=self.newFilePrompt)
         newButton.grid(row=0, column=0, padx=25, pady=(30,10))
         openButton = Button(self.root, text="Open", width=20, command=self.openFile)
@@ -41,46 +40,51 @@ class Start():
         deleteButton = Button(self.root, text="Delete", width=20, command=self.deleteFile)
         deleteButton.grid(row=2, column=0, padx=25, pady=10)
 
-
         #List of files
-
         addressBookListLabel = Label(self.root, text="Address Books")
-        addressBookListLabel.grid(row=0, column=1, padx=45)
+        addressBookListLabel.grid(row=0, column=1, padx=10, pady=10)
         scrollbar = Scrollbar(self.root, orient=VERTICAL)
-        self.addressBookList = Listbox(self.root, yscrollcommand=scrollbar.set, selectmode=SINGLE)
+        self.addressBookList = Listbox(self.root, yscrollcommand=scrollbar.set, selectmode=SINGLE, width=35)
         scrollbar.config(command=self.addressBookList.yview)
-        scrollbar.grid(column=2, row=1, rowspan=3, sticky="ns")
+        scrollbar.grid(column=2, row=1, rowspan=2, sticky="NS")
         for i in self.bookList:
             self.addressBookList.insert(END, i)
 
-        self.addressBookList.grid(row=1, column=1, rowspan=2, sticky="ns")
+        self.addressBookList.grid(row=1, column=1, rowspan=2, sticky="NS")
 
     def newFilePrompt(self):
         self.prompt = Toplevel(self.root)
         fileNameLabel = Label(self.prompt, text="Address Book Name")
-        fileNameLabel.grid(row=0, column=0)
+        fileNameLabel.grid(row=0, column=0, pady=5, padx=5)
         self.entry = Entry(self.prompt, bd=5)
-        self.entry.grid(row=0, column=1)
+        self.entry.grid(row=0, column=1, padx=5, pady=5)
         createButton = Button(self.prompt, text="Create", command=self.newFile)
-        createButton.grid(row=0, column=2)
+        createButton.grid(row=0, column=2, padx=3, pady=3)
 
     def newFile(self):
         self.fileName = self.entry.get()
         self.prompt.destroy()
         print(self.fileName)
-        self.bookList.append(self.fileName)
+        if (self.fileName not in self.bookList):
+            self.bookList.append(self.fileName)
+        else:
+            #display error
+            pass
         self.initializeUI()
-
 
     def openFile(self):
         fileIndex = self.addressBookList.curselection()
         print(fileIndex)
-        if fileIndex[0] != NONE:
+        if len(fileIndex) != 0:
             self.root.destroy()
             mainScreen = Window(self.bookList[fileIndex[0]])
 
     def deleteFile(self):
-        return
+        fileIndex = self.addressBookList.curselection()
+        if len(fileIndex) != 0:
+            os.remove("%s.db" % self.bookList[fileIndex[0]])
+            self.bookList = update_booklist()
+            self.initializeUI()
 
 
 class Window:
