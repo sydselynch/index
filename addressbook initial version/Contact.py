2c33205e83f8fe135dd929dec5328f93816f405c
jinjiel1994 <jinjiel1994@gmail.com>
2017-10-09 23:14:01 -0700

    Add files via upload
    implement address book

--- addressbook initial version/Contact.py
+++ addressbook initial version/Contact.py
@@ -1,10 +1,35 @@
 class Contact:
     def __init__(self, firstName, lastName, address, city, state, zipcode, phoneNumber, email):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zipcode = zipcode
-        self.phoneNumber = phoneNumber
-        self.email = email
+        self.FirstName = firstName
+        self.LastName = lastName
+        self.Address = address
+        self.City = city
+        self.State = state
+        self.Zipcode = zipcode
+        self.PhoneNumber = phoneNumber
+        self.Email = email
+
+    def GetFirstName(self):
+        return self.FirstName
+
+    def GetLastName(self):
+        return self.LastName
+
+    def GetAddress(self):
+        return self.Address
+
+    def GetCity(self):
+        return self.City
+
+    def GetState(self):
+        return self.State
+
+    def GetZipcode(self):
+        return self.Zipcode
+
+    def GetPhoneNumber(self):
+        return self.PhoneNumber
+
+    def GetEmail(self):
+        return self.Email
+
